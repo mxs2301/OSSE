165bb387ab90fe9055ccf693d18f5136dabaca1f
mxs2301 <max.schopf@mailfence.com>
2022-11-21 16:19:52 +0100

    Das was aus dem verdammten JavaDoc entstand

---
+++ praktikum/Docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"de.hfu","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"de.hfu","c":"AppTest","l":"AppTest()","u":"%3Cinit%3E()"},{"p":"de.hfu","c":"App","l":"greeter()"},{"p":"de.hfu","c":"App","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"de.hfu","c":"AppTest","l":"shouldAnswerWithTrue()"}];updateSearchResults();
