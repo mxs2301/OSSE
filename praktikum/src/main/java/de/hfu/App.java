0de761bd67c0ec7460ff57589e911c44f658eb0e
mxs2301 <max.schopf@mailfence.com>
2022-11-21 16:17:14 +0100

    Update um Stuff korrekt anzupassen

--- praktikum/src/main/java/de/hfu/App.java
+++ praktikum/src/main/java/de/hfu/App.java
@@ -3,11 +3,29 @@ package de.hfu;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ * Inputklasse
+ * Wandelt jegliche Zeichenkette in Großbuchstaben um
  *
+ *
+ * @author Max Michael Schopf
+ * @version 1.0
  */
 public class App 
 {
+
+
+    public static int greeter(){
+        System.out.println("Good bye");
+        /**
+         * @return 1;
+         */
+        return 1;
+    }
+
+    /**
+     * s
+     * @param args
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
@@ -16,5 +34,6 @@ public class App
         String input = scanner.nextLine();
         System.out.println(input.toUpperCase());
         scanner.close();
+
     }
 }
